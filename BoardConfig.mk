e94c2cce74e89734e739360cc6089c3c8f0d5e90
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    andromeda: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,9 @@ DEVICE_PATH := device/xiaomi/andromeda
 BOARD_VENDOR := xiaomi
 BUILD_BROKEN_DUP_RULES := true
 
+# A/B
+AB_OTA_UPDATER := false
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
