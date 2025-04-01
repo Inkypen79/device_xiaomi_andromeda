8bc4bc0b147299cfb8df73fb127e4e577ee32525
Nicholas Magill <nmagill1979@gmail.com>
2025-04-01 14:17:11 +0100

    andromeda: Add liblog dependency to libdirac.so
    It relies on __android_log_print symbol.

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo -e "gettid: 1" >> "${2}"
             ;;
+        vendor/lib/soundfx/libdirac.so)
+            grep -q liblog.so "${2}" || "${PATCHELF}" --add-needed "liblog.so" "${2}"
+            ;;
         vendor/lib64/libwvhidl.so)
             grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
