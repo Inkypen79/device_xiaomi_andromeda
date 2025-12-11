3300545975f56cd6d92783d2d541f0dfceaf06de
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 12:21:55 +0200

    andromeda: Patch qccsyshal to load versioned libprotobuf
    Change-Id: I9ec95bf55afd62a03b2bd787c8b82c15b921a452

--- extract-files.py
+++ extract-files.py
@@ -72,6 +72,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     (
     'vendor/etc/media_codecs.xml',
     'vendor/etc/media_codecs_vendor.xml',
